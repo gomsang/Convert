edb5b124f973abe1663b58e4ed7a5b1d7139b456
Jeonhui <dlwjsgml02@naver.com>
2022-02-12 22:25:58 +0900

    STYLE
    STYLE: sizeChecker
    Style fix

--- src/App.js
+++ src/App.js
@@ -1,56 +1,60 @@
-import React, {useState} from "react";
-import styled from "styled-components";
 import './App.css';
+import styled from "styled-components";
+import backgroundImage from "./background/background02.jpg";
+import Main from "./pages/main";
 
-const Select = styled.input`
-  font-size: 36px;
-  position: absolute;
-  top: 50px;
-  left: 125px;
-  width: 200px;
-`
-const Output = styled.div`
-  position: absolute;
-  top: 200px;
-  left: 150px;
-  font-size: 36px;
+const S = styled.span`
+  color: navajowhite;
 `
 
-const Base_size = styled.input`
-  font-size: 36px;
-  outline: none;
-  border: none;
-  border-bottom: darkgray solid 2px;
-  width: 150px;
+const BG01 = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 70%;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center center;
+  z-index: 0;
+`
+
+const Title = styled.div`
+  position: absolute;
+  top: 2rem;
+  left: 2.5rem;
+  font-weight: bold;
+  font-size: 2.5rem;
+  z-index: 10;
+`
+
+const Box = styled.div`
+  position: absolute;
+  width: 30%;
+  height: 100%;
+  min-width: 20rem;
+  right: 0;
+  background-color: white;
+`
+
+const Subtitle = styled.div`
+  display: inline-block;
+  position: absolute;
+  left: 50%;
+  bottom: 2rem;
+  width: 100%;
+  transform: translate(-50%,0);
+  color: dimgray;
+  text-align: center;
 `
 
 function App() {
-    const [find, setFind] = useState("")
-    //찾을 국가 선택 저장하는 텍스트 (inputBox에서 사용)
-
-    const [size, setSize] = useState(0)
-
-    const changeFind = (e) => {
-        setFind(e.target.value)
-    } //찾을 국가 변경 되면 find변수에 저장
-
-    const changeSize = (e) => {
-        setSize(e.target.value)
-    } //찾을 국가 변경 되면 find변수에 저장
-
-    const country_name = ["korea", "USA", "Italia"]
-    //선택할 국가 저장할 배열
-    const country = country_name.map((country) => <option value={country}/>)
-    //위 배열의 국가들을 option 태그 내에 반복 시킴 (datalist에 저장한 뒤에 input박스에 list로 전달하면 콤보박스 처럼 이용 가능)
-
     return (
         <div className="App">
-            <datalist id="country_list">{country}</datalist>
-            <Select type="text" value={find} onChange={changeFind} list="country_list"/>
-            <Output>
-                <Base_size type="text" value={size} onChange={changeSize}/>이
-                <span> {size}</span>으로 변환
-            </Output>
+            <BG01/>
+            <Title><S>Size</S> Checker@</Title>
+            <Box>
+                <Main/>
+                <Subtitle>made by Jeonhui & Gyeongrok</Subtitle>
+            </Box>
         </div>
     );
 }
---
+++ src/pages/main.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+import clothes from "../clothes_size.json";
+
+const Select = styled.input`
+  font-size: 36px;
+  position: absolute;
+  top: 50px;
+  left: 125px;
+  width: 200px;
+`
+const Output = styled.div`
+  position: absolute;
+  top: 200px;
+  left: 150px;
+  font-size: 36px;
+`
+
+const Base_size = styled.input`
+  font-size: 36px;
+  outline: none;
+  border: none;
+  border-bottom: darkgray solid 2px;
+  width: 150px;
+`
+
+function Main() {
+    const [find, setFind] = useState("")
+    //찾을 국가 선택 저장하는 텍스트 (inputBox에서 사용)
+
+    const [size, setSize] = useState(0)
+
+    const changeFind = (e) => {
+        setFind(e.target.value)
+    } //찾을 국가 변경 되면 find변수에 저장
+
+    const changeSize = (e) => {
+        setSize(e.target.value)
+    } //찾을 국가 변경 되면 find변수에 저장
+
+    const country_name = ["korea", "USA", "Italia"]
+    //선택할 국가 저장할 배열
+    const country = country_name.map((country) => <option value={country}/>)
+    //위 배열의 국가들을 option 태그 내에 반복 시킴 (datalist에 저장한 뒤에 input박스에 list로 전달하면 콤보박스 처럼 이용 가능)
+
+    return (
+        <div className="Main">
+            <datalist id="country_list">{country}</datalist>
+            <Select type="text" value={find} onChange={changeFind} list="country_list"/>
+            <Output>
+                <Base_size type="text" value={size} onChange={changeSize}/>이
+                <span> {size}</span>으로 변환
+            </Output>
+        </div>
+    );
+}
+
+export default Main;
