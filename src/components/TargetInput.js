aeb77f38fc0f1fc5aec381fcb052d73a9bf7b049
Jeonhui <dlwjsgml02@naver.com>
2022-02-17 13:12:41 +0900

    For gyeongrok

--- src/components/TargetInput.js
+++ src/components/TargetInput.js
@@ -11,10 +11,13 @@ function TargetInput() {
         <div id={"mainInputs"}>
             <input type="text" id="name" name="name" required
                    minLength="4" maxLength="8" size="10"/>
+
             <p className={"subText"}>to</p>
 
-            <select name="kinds" id="kinds">
-                {kinds.map(kind => <option value={kind}>{kind}</option>)}
+            <select name="kinds" id="kinds" onChange={(e)=>{setKind(e.target.value)
+            console.log(kind)}}>{/*추가 console.log는 너 확인 해보셈*/}
+                {kinds.map((kind,idx) => <option key={idx} value={kind}>{kind}</option>)}
+                {/*mapping할 때는 key값 넣어줘야 오류메시지 안 뜸*/}
             </select>
         </div>)
 }
