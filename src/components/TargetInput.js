2bbc84965612eaa4e44bccd2d4c8df2a43c11712
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2022-02-19 16:42:40 +0900

    feat: size & kind query function added

--- src/components/TargetInput.js
+++ src/components/TargetInput.js
@@ -1,28 +1,37 @@
 import "./TargetInput.css";
+import {queryKindByName} from "../utils/Size"
 
 import React, {useState} from "react";
+import QueryResultListItem from "./QueryResultListItem";
 
 function TargetInput() {
-    const [kind, setKind] = useState("")
-    const kinds = ["shoe-US(man)", "shoe-US(woman)", "shoe-JP", "shoe-EU(man)", "shoe-EU(woman)", "shoe-UK(man)", "shoe-UK(woman)"];
+    const [queryResult, setQueryResult] = useState([])
 
-    window.addEventListener("resize",(e)=>{
+    window.addEventListener("resize", (e) => {
         console.log(window.innerWidth)
     })
 
     return (
-        <div id={"mainInputs"}>
-            <input type="text" id="name" name="name" required
-                   minLength="4" maxLength="8" size="10"/>
-
-            <p className={"subText"}>to</p>
-
-            <select name="kinds" id="kinds" onChange={(e)=>{setKind(e.target.value)
-            console.log(kind)}}>{/*추가 console.log는 너 확인 해보셈*/}
-                {kinds.map((kind,idx) => <option key={idx} value={kind}>{kind}</option>)}
-                {/*mapping할 때는 key값 넣어줘야 오류메시지 안 뜸*/}
-            </select>
-        </div>)
+        <div>
+            <div id={"centerLayout"}>
+                <input type="text" id="name" name="name" required
+                       onChange={(event) => {
+                           let typedValue = event.target.value
+                           let queryResult = queryKindByName(typedValue)
+                           console.log(JSON.stringify(queryResult))
+                           setQueryResult(queryResult)
+                       }}
+                       minLength="4" maxLength="8" size="10"/>
+            </div>
+            <div id={"centerLayout"}>
+                <li id={"queryList"}>
+                    {queryResult.map(result => {
+                        return <ol><QueryResultListItem result={result}/></ol>
+                    })}
+                </li>
+            </div>
+        </div>
+    )
 }
 
 export default TargetInput
