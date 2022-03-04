0bc49e49b00a58a164dc030eeea7113ceb833343
Jeonhui <dlwjsgml02@naver.com>
2022-03-04 17:56:55 +0900

    fixed style

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -5,9 +5,12 @@ export default function Convert() {
 
     return (<div className={"Convert"}>
             <div className={"InputContainer"}>
-                <div className={"InputBox"}><span className={"letter"}>search</span><input type={"text"}/></div>
+                <div className={"InputBox"}><span className={"letter"}>search</span><input type={"text"} required
+                                                                                           minLength="4"
+                                                                                           maxLength="8" size="10"/>
+                </div>
                 <div className={"InputBox"}><span className={"letter"}>From<input type={"text"}/></span></div>
-                <div className={"InputBox"}><span className={"letter"}>To</span><input  type={"text"}/></div>
+                <div className={"InputBox"}><span className={"letter"}>To</span><input type={"text"}/></div>
             </div>
         </div>
     );
