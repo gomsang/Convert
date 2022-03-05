876e0b2f79915dc8eff06827fac238822d9fe253
Jeonhui <dlwjsgml02@naver.com>
2022-03-05 18:23:33 +0900

    add function

--- src/components/custom/TargetInput.js
+++ src/components/custom/TargetInput.js
@@ -9,36 +9,47 @@ export default function TargetInput() {
     const [recommendCursor, setRecommendCursor] = useState(0)
     let listRefs = []
     const ref = useRef()
+    const inputRef = useRef();
+    const _ = require('lodash');
 
     const onKeyDown = (e) => {
-        console.log(e.code);
         if (e.code === "ArrowDown") {
-            if (listRefs.length !== 0 && 0 < recommendCursor) {
-                setRecommendCursor(recommendCursor - 1);
-            }
-        } else if (e.code === "ArrowUp") {
             if (listRefs.length !== 0 && (listRefs.length - 1) > recommendCursor) {
                 setRecommendCursor(recommendCursor + 1);
             }
+        } else if (e.code === "ArrowUp") {
+            if (listRefs.length !== 0 && 0 < recommendCursor) {
+                setRecommendCursor(recommendCursor - 1);
+            } else if (recommendCursor === 0) {
+                setRecommendCursor(-1);
+                inputRef.current.focus();
+            }
         } else if (e.code === "Enter") {
+            console.log(recommendCursor);
             console.log(listRefs[recommendCursor].current);
+            console.log(listRefs);
         }
     }
 
     useEffect(() => {
         setQueryResult(querySizeByName(keyword));
+        console.log(typeof (ref))
     }, [keyword])
 
 
     return <div className={"formLayout"}>
-        <input className={"field"} type={"text"} value={keyword} onChange={(e) => {
-            setKeyword(e.target.value)
-            listRefs.splice(0, listRefs.length);
-        }} onKeyDown={onKeyDown}/>
+        <input className={"field"} type={"text"} value={keyword} ref={inputRef}
+               onChange={(e) => {
+                   setKeyword(e.target.value);
+                   listRefs.splice(0, listRefs.length);
+                   setRecommendCursor(-1);
+               }} onKeyDown={onKeyDown}/>
+
         <ol id={"queryList"}>
             {queryResult.map((result, idx) => {
-                    listRefs.push(ref);
-                    return (<li key={idx} ref={listRefs[idx]} className={recommendCursor===idx?"selectedItem":""}>
+                    listRefs.push(_.cloneDeep(ref));
+                    return (<li key={idx} ref={listRefs[idx]}
+                                className={idx === recommendCursor ? "selectedItem" : "notSelectedItem"}>
                         <QueryResultListItem result={result}/>
                     </li>)
                 }
