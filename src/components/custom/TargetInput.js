092417bc09ba4468641f4ab1968a537fba6ba53b
Jeonhui <dlwjsgml02@naver.com>
2022-03-05 18:02:06 +0900

    Update TargetInput.js

--- src/components/custom/TargetInput.js
+++ src/components/custom/TargetInput.js
@@ -6,19 +6,22 @@ import "./TargetInput.css";
 export default function TargetInput() {
     const [keyword, setKeyword] = useState("")
     const [queryResult, setQueryResult] = useState([])
+    const [recommendCursor, setRecommendCursor] = useState(0)
     let listRefs = []
     const ref = useRef()
 
     const onKeyDown = (e) => {
         console.log(e.code);
-        if (e.code==="ArrowDown"){
-
-        }
-        else if (e.code==="ArrowUp"){
-
-        }
-        else if(e.code==="Enter"){
-            console.log(listRefs[0].current);
+        if (e.code === "ArrowDown") {
+            if (listRefs.length !== 0 && 0 < recommendCursor) {
+                setRecommendCursor(recommendCursor - 1);
+            }
+        } else if (e.code === "ArrowUp") {
+            if (listRefs.length !== 0 && (listRefs.length - 1) > recommendCursor) {
+                setRecommendCursor(recommendCursor + 1);
+            }
+        } else if (e.code === "Enter") {
+            console.log(listRefs[recommendCursor].current);
         }
     }
 
@@ -35,7 +38,7 @@ export default function TargetInput() {
         <ol id={"queryList"}>
             {queryResult.map((result, idx) => {
                     listRefs.push(ref);
-                    return (<li key={idx} ref={listRefs[idx]}>
+                    return (<li key={idx} ref={listRefs[idx]} className={recommendCursor===idx?"selectedItem":""}>
                         <QueryResultListItem result={result}/>
                     </li>)
                 }
